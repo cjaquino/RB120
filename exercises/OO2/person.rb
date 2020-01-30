5497cad68daa2c026a7202135372396e7010fd2c
jex <jerritt.aquino@gmail.com>
2020-01-29 23:36:05 -0500

    OO2 exercise 10

--- exercises/OO2/person.rb
+++ exercises/OO2/person.rb
@@ -1,15 +1,19 @@
 class Person
     attr_writer :secret
   
-    def share_secret
-      puts secret
+    def compare_secret(other_person)
+      secret == other_person.secret
     end
 
-    private
+    protected
   
     attr_reader :secret
   end
   
   person1 = Person.new
   person1.secret = 'Shh.. this is a secret!'
-  person1.share_secret
+  
+  person2 = Person.new
+  person2.secret = 'Shh.. this is a different secret!'
+  
+  puts person1.compare_secret(person2)
