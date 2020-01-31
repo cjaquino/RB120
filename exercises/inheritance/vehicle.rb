47af5061c36d45ca5196b2f071176de248fc1cf0
jex <jerritt.aquino@gmail.com>
2020-01-30 23:01:31 -0500

    inheritance ex4

--- exercises/inheritance/vehicle.rb
+++ exercises/inheritance/vehicle.rb
@@ -1,23 +1,14 @@
 class Vehicle
-  attr_reader :year
-
-  def initialize(year)
-    @year = year
+  def start_engine
+    'Ready to go!'
   end
 end
 
 class Truck < Vehicle
-  attr_reader :bed_type
-
-  def initialize(year, bed_type)
-    super(year)
-    @bed_type = bed_type
+  def start_engine(speed)
+    super() + " Drive #{speed}, please!"
   end
 end
 
-class Car < Vehicle
-end
-
-truck1 = Truck.new(1994, 'Short')
-puts truck1.year
-puts truck1.bed_type
+truck1 = Truck.new
+puts truck1.start_engine('fast')
