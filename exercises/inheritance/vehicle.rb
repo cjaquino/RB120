729a13b4993d10baeeec58bc435774f08df41b62
jex <jerritt.aquino@gmail.com>
2020-01-30 23:06:10 -0500

    inheritance ex6

--- exercises/inheritance/vehicle.rb
+++ exercises/inheritance/vehicle.rb
@@ -1,14 +1,27 @@
+module Towable
+  def tow
+    'I can tow a trailer!'
+  end
+end
+
 class Vehicle
-  def start_engine
-    'Ready to go!'
+  attr_reader :year
+
+  def initialize(year)
+    @year = year
   end
 end
 
 class Truck < Vehicle
-  def start_engine(speed)
-    super() + " Drive #{speed}, please!"
-  end
+  include Towable
 end
 
-truck1 = Truck.new
-puts truck1.start_engine('fast')
+class Car < Vehicle
+end
+
+truck1 = Truck.new(1994)
+puts truck1.year
+puts truck1.tow
+
+car1 = Car.new(2006)
+puts car1.year
