319d2b80f42dc71a00696f4b3a68290c6f502483
cjaquino <jerritt.aquino@gmail.com>
2020-02-08 19:47:49 -0500

    initial spike for ttt game design

---
+++ lesson5/ttt.rb
@@ -0,0 +1,25 @@
+class Board
+  def initialize
+
+  end
+end
+
+class Square
+  def initialize
+
+  end
+end
+
+class Player
+  def initialize
+
+  end
+
+  def mark
+
+  end
+
+  def play
+  
+  end
+end
