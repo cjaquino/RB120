bbe0c7075f55fd4245e6c7dc95879490806f9366
cjaquino <jerritt.aquino@gmail.com>
2020-02-15 14:20:47 -0500

    further exploration - privacy

--- exercises/medium2/privacy.rb
+++ exercises/medium2/privacy.rb
@@ -7,9 +7,13 @@ class Machine
     flip_switch(:off)
   end
 
+  def switch_state
+    switch
+  end
+
   private
 
-  attr_writer :switch
+  attr_accessor :switch
 
   def flip_switch(desired_state)
     self.switch = desired_state
