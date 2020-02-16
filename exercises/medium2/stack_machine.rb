f81c8d495b77c1f83718d5ad74861f128575c75f
cjaquino <jerritt.aquino@gmail.com>
2020-02-16 16:25:51 -0500

    add temperature conversion commands

--- exercises/medium2/stack_machine.rb
+++ exercises/medium2/stack_machine.rb
@@ -101,41 +101,51 @@ class Minilang
   end
 end
 
-# CENTIGRADE_TO_FAHRENHEIT = '5 PUSH %<degrees_c>d PUSH 9 MULT DIV PUSH 32 ADD PRINT'
+CENTIGRADE_TO_FAHRENHEIT = '5 PUSH %<degrees_c>d PUSH 9 MULT DIV PUSH 32 ADD PRINT'
+c_to_f = Minilang.new(CENTIGRADE_TO_FAHRENHEIT)
+c_to_f.eval(degrees_c: 100)
+c_to_f.eval(degrees_c: 0)
+c_to_f.eval(degrees_c: -40)
+
+FAHR_TO_CENT = '9 PUSH 32 PUSH %<degrees_f>d SUB PUSH 5 MULT DIV PRINT'
+f_to_c = Minilang.new(FAHR_TO_CENT)
+f_to_c.eval(degrees_f: 212)
+f_to_c.eval(degrees_f: 32)
+f_to_c.eval(degrees_f: -40)
 
 
-Minilang.new('PRINT').eval
-# 0
+# Minilang.new('PRINT').eval
+# # 0
 
-Minilang.new('5 PUSH 3 MULT PRINT').eval
-# 15
+# Minilang.new('5 PUSH 3 MULT PRINT').eval
+# # 15
 
-Minilang.new('5 PRINT PUSH 3 PRINT ADD PRINT').eval
-# 5
-# 3
-# 8
+# Minilang.new('5 PRINT PUSH 3 PRINT ADD PRINT').eval
+# # 5
+# # 3
+# # 8
 
-Minilang.new('5 PUSH 10 PRINT POP PRINT').eval
-# 10
-# 5
+# Minilang.new('5 PUSH 10 PRINT POP PRINT').eval
+# # 10
+# # 5
 
-Minilang.new('5 PUSH POP POP PRINT').eval
-# Empty stack!
+# Minilang.new('5 PUSH POP POP PRINT').eval
+# # Empty stack!
 
-Minilang.new('3 PUSH PUSH 7 DIV MULT PRINT ').eval
-# 6
+# Minilang.new('3 PUSH PUSH 7 DIV MULT PRINT ').eval
+# # 6
 
-Minilang.new('4 PUSH PUSH 7 MOD MULT PRINT ').eval
-# 12
+# Minilang.new('4 PUSH PUSH 7 MOD MULT PRINT ').eval
+# # 12
 
-Minilang.new('-3 PUSH 5 XSUB PRINT').eval
-# Invalid token: XSUB
+# Minilang.new('-3 PUSH 5 XSUB PRINT').eval
+# # Invalid token: XSUB
 
-Minilang.new('-3 PUSH 5 SUB PRINT').eval
-# 8
+# Minilang.new('-3 PUSH 5 SUB PRINT').eval
+# # 8
 
-Minilang.new('6 PUSH').eval
-# (nothing printed; no PRINT commands)
+# Minilang.new('6 PUSH').eval
+# # (nothing printed; no PRINT commands)
 
 
-Minilang.new('6 PUSH').eval
+# Minilang.new('6 PUSH').eval
