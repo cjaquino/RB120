d4bc4211e0dc36d334d3d6304010356641c5cbe3
cjaquino <jerritt.aquino@gmail.com>
2020-02-17 22:05:38 -0500

    hilo initial solution

--- exercises/medium2/hilo.rb
+++ exercises/medium2/hilo.rb
@@ -1,9 +1,25 @@
 class Card
+  FACE_VALUES = {'Jack' => 11, 'Queen' => 12, 'King' => 13, 'Ace' => 14}
+
   attr_reader :rank, :suit
+
+  include Comparable
 
   def initialize(rank, suit)
     @rank = rank
     @suit = suit
+  end
+
+  def to_s
+    "#{rank} of #{suit}"
+  end
+
+  def <=>(other)
+    rank_value <=> other.rank_value
+  end
+
+  def rank_value
+    FACE_VALUES.fetch(rank, rank)
   end
 end
 
