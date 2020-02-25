1a88078eb99b7ceef43fdc5e00acb8afd0fe320b
cjaquino <jerritt.aquino@gmail.com>
2020-02-24 20:32:02 -0500

    cgol on console v1

--- ConwayGoL/game.rb
+++ ConwayGoL/game.rb
@@ -1,4 +1,6 @@
 class Grid
+  attr_reader :squares
+
   def initialize(x = 5, y = 5)
     @squares = empty_squares(x, y)
   end
@@ -19,6 +21,18 @@ class Grid
       puts ""
     end
   end
+
+  def revive(y, x)
+    @squares[y][x].fill
+  end
+
+  def kill(coord)
+    @squares[y][x].empty
+  end
+
+  def toggle(y, x)
+    @squares[y][x].toggle
+  end
 end
 
 class Square
@@ -32,16 +46,56 @@ class Square
   def to_s
     " #{@state} "
   end
+
+  def fill
+    @state = FILLED_SIGN
+  end
+
+  def empty
+    @state = EMPTY_SIGN
+  end
+
+  def toggle
+    @state = (@state == FILLED_SIGN ? EMPTY_SIGN : FILLED_SIGN)
+  end
 end
 
 class CGoL
+  attr_reader :grid
+
   def initialize
     @grid = Grid.new
   end
 
   def play
-    @grid.display
+    3.times do |_|
+      clear_screen
+      @grid.display
+      @grid.toggle(0,0)
+      clear_screen
+      @grid.display
+      @grid.toggle(0,0)
+      @grid.toggle(0,1)
+      clear_screen
+      @grid.display
+      @grid.toggle(0,1)
+      @grid.toggle(0,2)
+      clear_screen
+      @grid.display
+      @grid.toggle(0,2)
+      @grid.toggle(0,3)
+      clear_screen
+      @grid.display
+      @grid.toggle(0,3)
+      @grid.toggle(0,4)
+    end
+  end
+
+  def clear_screen
+    sleep(0.1)
+    system("clear")
   end
 end
 
-CGoL.new.play
+game = CGoL.new
+game.play
